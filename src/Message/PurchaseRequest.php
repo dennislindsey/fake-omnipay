7d6f07cb96a400b53a4ce2ab083aae9841d7cd0c
Dennis <dennis@infostreamgroup.com>
2017-06-23 16:27:18 -0700

    Add some more setters on PurchaseRequest

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -7,12 +7,23 @@ use Omnipay\Common\Message\AbstractRequest;
 class PurchaseRequest extends AbstractRequest
 {
 
-    public function sendData($data = [])
+    /**
+     * Send the purchase request
+     *
+     * @param array $data
+     * @return PurchaseResponse
+     */
+    public function sendData($data = []): PurchaseResponse
     {
         return $this->response = new PurchaseResponse($this, $this->getResponseData());
     }
 
-    public function getResponseData()
+    /**
+     * Get the response data
+     *
+     * @return array
+     */
+    public function getResponseData(): array
     {
         return [
             'amount_usd' => $this->getParameters()['amount'],
@@ -20,8 +31,58 @@ class PurchaseRequest extends AbstractRequest
         ];
     }
 
-    public function getData()
+    /**
+     * Get the purchase parameters
+     *
+     * @return array
+     */
+    public function getData(): array
     {
         return $this->getParameters();
     }
+
+    /**
+     * Sets the payment schema
+     *
+     * @param $value
+     * @return AbstractRequest
+     */
+    public function setPaymentSchema($value): AbstractRequest
+    {
+        return $this->setParameter('paymentSchema', $value);
+    }
+
+    /**
+     * Sets the payment frequency
+     *
+     * @param $value
+     * @return AbstractRequest
+     */
+    public function setFrequency($value): AbstractRequest
+    {
+        return $this->setParameter('paymentSchema', $value);
+    }
+
+    /**
+     * Sets the payment interval
+     *
+     * @param $value
+     * @return AbstractRequest
+     */
+    public function setInterval($value): AbstractRequest
+    {
+        return $this->setParameter('interval', $value);
+    }
+
+    /**
+     * Sets the payment start date
+     *
+     * @param $value
+     * @return AbstractRequest
+     */
+    public function setStartDate($value): AbstractRequest
+    {
+        return $this->setParameter('startDate', $value);
+    }
+
 }
