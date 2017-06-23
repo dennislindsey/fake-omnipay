09d647a6472be66e19383fb848f56aa2736b4231
Dennis <dennis@infostreamgroup.com>
2017-06-23 14:40:58 -0700

    Updating data in PurchaseRequest

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -9,10 +9,19 @@ class PurchaseRequest extends AbstractRequest
 
     public function sendData($data = [])
     {
-        return $this->response = new PurchaseResponse($this, []);
+        return $this->response = new PurchaseResponse($this, $this->getResponseData());
+    }
+
+    public function getResponseData()
+    {
+        return [
+            'amount_usd' => $this->getParameters()['amount'],
+            'currency'   => $this->getParameters()['currency'],
+        ];
     }
 
     public function getData()
     {
+        return $this->getParameters();
     }
 }
